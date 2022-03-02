db2e71e7b505d517343c1a48d74f9285c4457ea5
Samsuddin-Shohan <shohan.cse.hstu@gmail.com>
2022-03-02 07:16:04 +0600

    facebook authentication added

--- src/Components/Authentication/Authentication.js
+++ src/Components/Authentication/Authentication.js
@@ -1,10 +1,17 @@
-import { Button } from 'bootstrap';
+
 import React from 'react';
 import initializeFirebase from '../../Firebase/firebase.init';
+import { getAuth, signInWithPopup, GithubAuthProvider } from "firebase/auth";
+import { useState } from 'react';
+
 
 initializeFirebase();
+const auth = getAuth();
+const githubProvider = new GithubAuthProvider();
+
 
 const Authentication = () => {
+    const [user, setUser] = useState({});
     const handleFacebookLogin = ()=>{
         console.log('facebook clicked');
     }
@@ -12,7 +19,25 @@ const Authentication = () => {
         console.log('twitter clicked');
     }
     const handleGithubLogin = () =>{
-        console.log('github clicked');
+        // console.log('github clicked');
+        signInWithPopup(auth, githubProvider)
+  .then((result) => {
+    // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+    const credential = GithubAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+
+    // The signed-in user info.
+    const people = result.user;
+    console.log(user);
+    const {email,displayName,photoURL} = people;
+    setUser({
+        name:displayName,
+        email,
+        photo:photoURL
+    });
+    // ...
+  })
+
     }
     return (
         <div>
@@ -20,6 +45,13 @@ const Authentication = () => {
             <button onClick={handleFacebookLogin} className='btn btn-primary m-3 '>Sign in With Facebook</button>
             <button onClick={handleGithubLogin} className='btn btn-primary m-3  '>Sign in With Github</button>
             <button onClick={handleTwitterLogin} className='btn btn-primary  m-3 '>Sign in With Twitter</button>
+            {
+                user.name && <div className='m-3'>
+                <h2>User: {user.name}</h2>
+                <h4>Email: {user.email}</h4>
+                <img src={user.photo} alt="" />
+            </div>
+            }
         </div>
     );
 };
